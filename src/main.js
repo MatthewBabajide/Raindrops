cdd0fc111811c43da78fb8c2f1d62733311604af
MatthewBabajide <32984917+MatthewBabajide@users.noreply.github.com>
2017-10-27 11:10:04 +0100

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,21 +1,24 @@
+/**
+ * @param {Number} num
+ * @returns {Number} num
+ */
+const rainDrops = (num) => {
+  let code = '';
 
-const rainDrops= (num) => { 
-    let code= '';
-
-      if (!Number(num)) return 'invalid input please input a number';
-         if (num % 3 === 0) {
-            code += "Pling";
-          }
-          if (num % 5 === 0) {
-            code +=  "Plang";
-          }
-          if (num % 7 === 0) {
-            code += "Plong";
-          }
-          if (code.length ===0) {
-            return Number(num);
-          }
-        return code;
-    }   
+  if (!Number(num)) return 'invalid input please input a number';
+  if (num % 3 === 0) {
+    code += 'Pling';
+  }
+  if (num % 5 === 0) {
+    code += 'Plang';
+  }
+  if (num % 7 === 0) {
+    code += 'Plong';
+  }
+  if (code.length === 0) {
+    return Number(num);
+  }
+  return code;
+};
 
 export default rainDrops;
--- test/test.js
+++ test/test.js
@@ -1,45 +1,42 @@
-//const assert = require('chai').assert;
 
-//const myApp = require('../src/main.js');
-
-import { assert }from 'chai';
-import rainDrops from '../src/main.js';
+import { assert } from 'chai';
+import rainDrops from '../src/main';
 
 
-describe("rainDrops", () => {
-   describe("rainDrops tests", () => {
-       it("should return Plong for 28", () => {
-           assert.equal(rainDrops(28), 'Plong');
-       });
-       it("should return 13 for 13", () => {
-           assert.equal(rainDrops(13), 13);
-       });
-       it("should return PlingPlang for 1755", () => {
-           assert.equal(rainDrops(1755), 'PlingPlang');
-       });
-        it("should return 34 for 34", () => {
-           assert.equal(rainDrops(34), '34');
-       });
-        it("should return invalid input please input a number for ''", () => {
-           assert.equal(rainDrops (''), 'invalid input please input a number');
-       });
-         it("should return invalid input please input a number for 0", () => {
-           assert.equal(rainDrops(0), 'invalid input please input a number');
-       });
-       it("should return 4.6 for 4.6", () => {
-           assert.equal(rainDrops(4.6), '4.6');
-       });
-       it("should return 34 for '34'", () => {
-           assert.equal(rainDrops('34'), '34');
-       });
-       it("should return PlingPlangPlong for 105", () => {
-           assert.equal(rainDrops(105), 'PlingPlangPlong');
-       });
-        it("should return invalid input please input a number for 'Hello'", () => {
-           assert.equal(rainDrops('Hello'), 'invalid input please input a number');
-       });
-       it("should return Plang for 65", () => {
-           assert.equal(rainDrops(65), 'Plang');
-         });   
-   })
-})
+describe('rainDrops', () => {
+  describe('rainDrops tests', () => {
+    it('should return Plong for 28', () => {
+      assert.equal(rainDrops(28), 'Plong');
+    });
+    it('should return 13 for 13', () => {
+      assert.equal(rainDrops(13), 13);
+    });
+    it('should return PlingPlang for 1755', () => {
+      assert.equal(rainDrops(1755), 'PlingPlang');
+    });
+    it('should return 34 for 34', () => {
+      assert.equal(rainDrops(34), '34');
+    });
+    it("should return invalid input please input a number for ''", () => {
+      assert.equal(rainDrops(''), 'invalid input please input a number');
+    });
+    it('should return invalid input please input a number for 0', () => {
+      assert.equal(rainDrops(0), 'invalid input please input a number');
+    });
+    it('should return 4.6 for 4.6', () => {
+      assert.equal(rainDrops(4.6), '4.6');
+    });
+    it("should return 34 for '34'", () => {
+      assert.equal(rainDrops('34'), '34');
+    });
+    it('should return PlingPlangPlong for 105', () => {
+      assert.equal(rainDrops(105), 'PlingPlangPlong');
+    });
+    it("should return invalid input please input a number for 'Hello'", () => {
+      assert.equal(rainDrops('Hello'), 'invalid input please input a number');
+    });
+    it('should return Plang for 65', () => {
+      assert.equal(rainDrops(65), 'Plang');
+    });
+  });
+});
